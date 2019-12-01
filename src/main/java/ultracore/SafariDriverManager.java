7f740c5ca608ca2cfc0aa9961764ae40d4cfde91
mrntisana <46403104+mrntisana@users.noreply.github.com>
2019-12-01 04:22:56 +0200

    Created SafariDriverManager

---
+++ src/main/java/ultracore/SafariDriverManager.java
@@ -0,0 +1,13 @@
+package ultracore;
+
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
+public class SafariDriverManager extends DriverManager {
+    @Override
+    protected void createWebDriver() {
+        SafariOptions options = new SafariOptions();
+
+        this.driver = new SafariDriver(options);
+    }
+}
