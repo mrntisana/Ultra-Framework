5cc9c320e8f839c85d8b6ad211b2701e065675ab
mrntisana <46403104+mrntisana@users.noreply.github.com>
2019-12-01 04:17:35 +0200

    Created EdgeDriverManager

---
+++ src/main/java/ultracore/EdgeDriverManager.java
@@ -0,0 +1,13 @@
+package ultracore;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
+public class EdgeDriverManager extends DriverManager {
+    @Override
+    protected void createWebDriver() {
+        EdgeOptions options = new EdgeOptions();
+
+        this.driver = new EdgeDriver(options);
+    }
+}
